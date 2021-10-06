10b280b03cb0a26f81527d14d7366cd1077e2f3f
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2021-10-06 16:29:21 +0200

    username and roomid gets saved to variable
    username and roomid gets saved to variable and can be turned into a global variable

--- src/Home/ReactApp.tsx
+++ src/Home/ReactApp.tsx
@@ -1,29 +1,23 @@
 import React from 'react';
-import TextField from '@mui/material/TextField';
-import { styled } from '@mui/material/styles';
-import { purple } from '@mui/material/colors';
-import Button, { ButtonProps } from '@mui/material/Button';
-import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
-import { Card, CardContent, CardHeader, CardActions, Typography } from "@mui/material";
+import SignInHook from "./SignInHook";
+
+
+
 
 class ReactApp extends React.Component {
 
+    
+    handleClick() {
+
+        console.log('You clicked submit.');
+    }
 
     render() {
+        
         console.log("Hello");
         return (
+            <SignInHook></SignInHook>
             
-            <Card sx={{width: "25%", bgcolor: "lightGray", height: "50%", margin: "0 auto"}}>
-                <Typography sx={{ fontSize: 24, fontWeight: 'bold', padding: "5%", textAlign: 'center' }} >SPV Hybrid Meetings</Typography>
-                <CardContent>
-                    <Typography sx={{ fontSize: 12 }}  color="text.secondary">Please enter your information</Typography>
-                    <TextField className="inputBox" label="Username" variant="outlined" required></TextField>
-                    <TextField className="inputBox" label="Room ID" variant="outlined" required></TextField>
-                </CardContent>
-                <CardActions>
-                    <PurpleButton variant="contained" color="success">CONTINUE<DoubleArrowIcon/></PurpleButton>
-                </CardActions>
-            </Card>
             
         );
 
@@ -32,14 +26,6 @@ class ReactApp extends React.Component {
     
 };
 
-const PurpleButton = styled(Button)<ButtonProps>(({ theme }) => ({
-    color: theme.palette.getContrastText(purple[500]),
-    margin: "0% 20%",
-    backgroundColor: purple[500],
-    '&:hover': {
-      backgroundColor: purple[700],
-    },
-  }));
 
 
 export default ReactApp;
