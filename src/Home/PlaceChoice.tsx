c67c3a9999b5ad31f92794341578d636d7d4a808
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2021-10-07 20:31:20 +0200

    Home/office view
    functions that call office or home UI, no functionality yet

--- src/Home/PlaceChoice.tsx
+++ src/Home/PlaceChoice.tsx
@@ -2,43 +2,66 @@ import React from 'react';
 import { Link, Redirect, useHistory } from "react-router-dom"
 import Button, { ButtonProps } from '@mui/material/Button'
 import { styled } from '@mui/material/styles';
-import { purple } from '@mui/material/colors';
-import { Card, CardContent, CardActions, Typography } from "@mui/material";
-import TextField from '@mui/material/TextField';
+import { blue, purple, grey } from '@mui/material/colors';
+import { Box, Card, CardContent, CardActions, Typography, Paper, PaperProps } from "@mui/material";
+import CenterRipple from '@mui/material/ButtonBase'
+import BusinessRoundedIcon from '@mui/icons-material/BusinessRounded';
+import HomeWorkRoundedIcon from '@mui/icons-material/HomeWorkRounded';
 
 
 
+const ChoiceButton = styled(Button)<ButtonProps>(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    margin: "0% 20%",
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    }, 
+  }));
 
 
 
 class PlaceChoice extends React.Component {
-    
+    testPaper = styled(Paper)(({theme}) => ({
+        ...theme.typography.body2,
+        textAlign: 'center',
+        backgroundColor: grey[300],
+        lineHeight: '60px',
+        height: '100%',
+        width: '100%',
+        '&:hover': {
+          backgroundColor: purple[500],
+        },
+        '&:active': {
+            backgroundColor: purple[800],
+        },
+    }));
 
-    validateEntry() {
-        console.log("Username in PlaceChoice is: " )
+    goToOffice(){
+        console.log("office!")
+    }
+    goToHome(){
+        console.log("Home!")
     }
 
     render() {
         
         
         return (
-            <Card sx={{width: "50%", bgcolor: "lightGray", height: "50%", margin: "0 auto"}}>
-                <Typography sx={{ fontSize: 24, fontWeight: 'bold', padding: "5%", textAlign: 'center' }} >SPV Hybrid Meetings</Typography>
-                <CardContent>
-                    <Typography sx={{ fontSize: 12 }}  color="text.secondary">Please enter your information</Typography>
-                    <TextField 
-                        className="inputBox" 
-                        label="Username" 
-                        variant="outlined">
-                        </TextField>
-                    <TextField 
-                        className="inputBox" 
-                        label="Room ID" 
-                        variant="outlined">
-                        </TextField>
-                        <Button onClick={this.validateEntry}>hej</Button>
-                </CardContent>
-            </Card>
+            <Box sx={{width: "auto", height: "auto", display: 'flex', alignItems: "center", justifyContent: "center", flexWrap: 'wrap',
+            '& > :not(style)': {
+              m: 5,
+              width: 150,
+              height: 150,
+            }, }}>
+                <CenterRipple>
+                    <this.testPaper elevation={5} onClick={this.goToOffice}><BusinessRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><Typography sx={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', color: "black" }}>Office</Typography></this.testPaper>
+                </CenterRipple>   
+                <CenterRipple> 
+                    <this.testPaper elevation={5} onClick={this.goToHome}><HomeWorkRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><Typography sx={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', color: "black" }}>Home</Typography></this.testPaper>
+                </CenterRipple>
+            
+            </Box>
             
            
             
@@ -52,3 +75,38 @@ class PlaceChoice extends React.Component {
 
 
 export default PlaceChoice;
+
+/* 
+<Card sx={{width: "25%", bgcolor: "lightGray", height: "50%", margin: "0 auto"}}>
+                    <Typography sx={{ fontSize: 24, fontWeight: 'bold', padding: "5%", textAlign: 'center' }} >SPV Hybrid Meetings</Typography>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 12 }}  color="text.secondary">Please enter your information</Typography>
+                        <TextField 
+                            className="inputBox" 
+                            label="Username" 
+                            variant="outlined">
+                            </TextField>
+                        <TextField 
+                            className="inputBox" 
+                            label="Room ID" 
+                            variant="outlined">
+                            </TextField>
+                    </CardContent>
+                </Card>
+                <Card sx={{width: "25%", bgcolor: "lightGray", height: "50%", margin: "0 auto"}}>
+                    <Typography sx={{ fontSize: 24, fontWeight: 'bold', padding: "5%", textAlign: 'center' }} >SPV Hybrid Meetings</Typography>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 12 }}  color="text.secondary">Please enter your information</Typography>
+                        <TextField 
+                            className="inputBox" 
+                            label="Username" 
+                            variant="outlined">
+                            </TextField>
+                        <TextField 
+                            className="inputBox" 
+                            label="Room ID" 
+                            variant="outlined">
+                            </TextField>
+                    </CardContent>
+                </Card>
+*/ 
