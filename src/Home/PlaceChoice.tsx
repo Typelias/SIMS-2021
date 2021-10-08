3ade9566064e8a4df44e88703d145dad6c60d811
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2021-10-08 09:49:43 +0200

    OfficeChoice added

--- src/Home/PlaceChoice.tsx
+++ src/Home/PlaceChoice.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link, Redirect, useHistory } from "react-router-dom"
-import Button, { ButtonProps } from '@mui/material/Button'
+import { useHistory } from "react-router-dom"
 import { styled } from '@mui/material/styles';
 import { blue, purple, grey } from '@mui/material/colors';
 import { Box, Card, CardContent, CardActions, Typography, Paper, PaperProps } from "@mui/material";
@@ -9,10 +8,11 @@ import BusinessRoundedIcon from '@mui/icons-material/BusinessRounded';
 import HomeWorkRoundedIcon from '@mui/icons-material/HomeWorkRounded';
 
 
-
+const history = useHistory();
 
 
 class PlaceChoice extends React.Component {
+    
     testPaper = styled(Paper)(({theme}) => ({
         ...theme.typography.body2,
         textAlign: 'center',
@@ -21,7 +21,7 @@ class PlaceChoice extends React.Component {
         height: '100%',
         width: '100%',
         '&:hover': {
-          backgroundColor: purple[500],
+          backgroundColor: purple[300],
         },
         '&:active': {
             backgroundColor: purple[800],
@@ -30,9 +30,11 @@ class PlaceChoice extends React.Component {
 
     goToOffice(){
         console.log("office!")
+        history.push("/OfficeChoice")
     }
     goToHome(){
         console.log("Home!")
+        history.push("/HomeUi")
     }
 
     render() {
