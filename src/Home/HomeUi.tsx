702a6e6e7a61ae4d5ab65c5ac38f8f4686388e30
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2021-10-10 21:09:09 +0200

    ny camview component

--- src/Home/HomeUi.tsx
+++ src/Home/HomeUi.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import HomeCameraView from '../CameraView/HomeCameraView';
 
 
 class HomeUi extends React.Component  {
 
     render(){
         return(
-            <div><h1>This is Home!</h1></div>
+            <div>
+                <h1>This is Home!</h1>
+                <HomeCameraView/>
+            </div>
+            
+
         );
     }
 
