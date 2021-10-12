eb13d46a30d416f67d87af76adc128153f0aeec8
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2021-10-12 13:33:24 +0200

    home or office pick is now fixed and use styled component

--- src/Home/HomeOfficeHook.jsx
+++ src/Home/HomeOfficeHook.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { useHistory } from "react-router-dom"
-import { styled } from '@mui/material/styles';
-import { blue, purple, grey } from '@mui/material/colors';
+import  styled  from 'styled-components'
 import { Box, Typography, Paper} from "@mui/material";
 import CenterRipple from '@mui/material/ButtonBase'
 import BusinessRoundedIcon from '@mui/icons-material/BusinessRounded';
@@ -12,23 +11,28 @@ function HookTest() {
 
     let history = useHistory()
 
-    const TestPaper = styled(Paper)(({theme}) => ({
-        ...theme.typography.body2,
-        textAlign: 'center',
-        backgroundColor: grey[300],
-        lineHeight: '60px',
-        height: '100%',
-        width: '100%',
-        cursor: 'pointer',
-        transition: 'transform ease 300ms',
-        '&:hover': {
-            backgroundColor: purple[300],
-            transform: 'translate(0, -10px)',
-        },
-        '&:active': {
-            backgroundColor: purple[800],
-        },
-    }));
+    const PaperStyle = styled(Paper)`
+        text-align: center;
+        background-color: ${({ theme }) => theme.colorGrey};
+        line-height: 60px;
+        height: 100%;
+        width: 100%;
+        cursor: pointer;
+        transition: transform ease 300ms;
+        &:hover {
+            background-color: ${({ theme }) => theme.headerNumber};
+            transform: translate(0, -10px);
+        }
+        &:active {
+            background-color: ${({ theme }) => theme.colorBlack};
+        }
+    `
+    const TypographyStyle = styled(Typography)`
+        font-size: 24;
+        font-weight: bold;
+        text-align: center;
+        color: black;
+    `
     
     function goToOffice(){
 
@@ -49,10 +53,10 @@ function HookTest() {
               height: 150,
             }, }}>
                 <CenterRipple>
-                    <TestPaper elevation={5} onClick={goToOffice}><BusinessRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><Typography sx={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', color: "black" }}>Office</Typography></TestPaper>
+                    <PaperStyle elevation={5} onClick={goToOffice}><BusinessRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><TypographyStyle>Office</TypographyStyle></PaperStyle>
                 </CenterRipple>   
                 <CenterRipple> 
-                    <TestPaper elevation={5} onClick={goToHome}><HomeWorkRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><Typography sx={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', color: "black" }}>Home</Typography></TestPaper>
+                    <PaperStyle elevation={5} onClick={goToHome}><HomeWorkRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><TypographyStyle>Home</TypographyStyle></PaperStyle>
                 </CenterRipple>    
             
         </Box>
--- src/UI/Styles/Theme.jsx
+++ src/UI/Styles/Theme.jsx
@@ -9,6 +9,7 @@ const globalTheme = {
     gradient: 'linear-gradient(122deg, rgba(128,74,216,1) 0%, rgba(98,75,217,1) 100%)',
     colorGreen: '#5DC399',
     colorGray: '#adadad',
+    colorBlack: '#000'
 }
 
 
