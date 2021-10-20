69055a30ab78fb933ba3c6b574bc130c682c3ae2
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2021-10-20 14:42:46 +0200

    mkt som händer nu

--- src/Home/HomeOfficeHook.jsx
+++ src/Home/HomeOfficeHook.jsx
@@ -5,6 +5,10 @@ import { Box, Typography, Paper} from "@mui/material";
 import CenterRipple from '@mui/material/ButtonBase'
 import BusinessRoundedIcon from '@mui/icons-material/BusinessRounded';
 import HomeWorkRoundedIcon from '@mui/icons-material/HomeWorkRounded';
+import Grow from '@mui/material/Grow';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
 
 const PaperStyle = styled(Paper)`
     text-align: center;
@@ -24,11 +28,13 @@ const PaperStyle = styled(Paper)`
 `
 
 function HookTest() {
+    const [checked, setChecked] = React.useState(false);
 
     let history = useHistory()
 
     function goToOffice(){
-        history.push("/OfficeUi")
+        setChecked((prev) => !prev);
+        
     }
     function goToHome(){ 
         history.push("/HomeUi")
@@ -47,23 +53,34 @@ function HookTest() {
     
 
     return (
+        <div>
         <Box sx={{width: "auto", height: "auto", display: 'flex', alignItems: "center", justifyContent: "center", flexWrap: 'wrap',
             '& > :not(style)': {
               m: 5,
               width: 150,
               height: 150,
             }, }}>
+                
                 <CenterRipple>
-                    <PaperStyle elevation={5} onClick={goToOffice}><BusinessRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><TypographyStyle>Office</TypographyStyle></PaperStyle>
+                <PaperStyle elevation={5} onClick={goToOffice}><BusinessRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><TypographyStyle>Office</TypographyStyle></PaperStyle>
                 </CenterRipple>   
+                <Box sx={{ position: 'absolute', display: 'flex', alignItems: "bottom", paddingTop: '8%', paddingRight: '10%'}}>
+                    <Grow sx={{ width: 100, height: 100 }} in={checked}><AddCircleIcon/></Grow>
+                    <Grow sx={{ width: 100, height: 100 }} in={checked}><AddCircleIcon/></Grow>
+
+                </Box>
+                
                 <CenterRipple> 
                     <PaperStyle elevation={5} onClick={goToHome}><HomeWorkRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><TypographyStyle>Home</TypographyStyle></PaperStyle>
                 </CenterRipple>    
             
         </Box>
         
+        </div>
+        
     )
     
 
 }
 export default HookTest
+/*history.push("/OfficeUi")*/
