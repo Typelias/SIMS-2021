1367fd8bbe3f7d43ad1f9ee7bb2caab268e2ce69
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2021-10-20 19:07:17 +0200

    options for meeting Leader and participant as a dropdown menu
    not so nicely scaled xD

--- src/Home/HomeOfficeHook.jsx
+++ src/Home/HomeOfficeHook.jsx
@@ -1,46 +1,77 @@
 import React from 'react'
 import { useHistory} from "react-router-dom"
-import  styled  from 'styled-components'
+import styled from 'styled-components'
 import { Box, Typography, Paper} from "@mui/material";
 import CenterRipple from '@mui/material/ButtonBase'
 import BusinessRoundedIcon from '@mui/icons-material/BusinessRounded';
 import HomeWorkRoundedIcon from '@mui/icons-material/HomeWorkRounded';
+import { Icon } from '@iconify/react';
 import Grow from '@mui/material/Grow';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+
 
 
 const PaperStyle = styled(Paper)`
     text-align: center;
     background-color: ${({ theme }) => theme.colorGrey};
     line-height: 60px;
-    height: 100%;
-    width: 100%;
+    margin-right: 20%;
+    padding: 5%;
+    width: 25vh;
+    height: 18vh;
     cursor: pointer;
     transition: transform ease 300ms;
-&:hover {
-    background-color: ${({ theme }) => theme.headerNumber};
-    transform: translate(0, -10px);
-}
-&:active {
-    background-color: ${({ theme }) => theme.colorBlack};
-}
+    &:hover {
+        background-color: ${({ theme }) => theme.headerNumber};
+        transform: translate(0, -10px);
+    }
+    &:active {
+        background-color: ${({ theme }) => theme.colorBlack};
+    }
+`
+
+const SmallPaper = styled(Paper)`
+    text-align: center;
+    background-color: ${({ theme }) => theme.colorGrey};
+    line-height: 60px;
+    margin-right: 10%;
+    padding: 5%;
+   
+    border-radius: 30px;
+    cursor: pointer;
+    transition: transform ease 300ms;
+    &:hover {
+        background-color: ${({ theme }) => theme.headerNumber};
+        transform: translate(0, -10px);
+    }
+    &:active {
+        background-color: ${({ theme }) => theme.colorBlack};
+    }
 `
 
 function HookTest() {
     const [checked, setChecked] = React.useState(false);
 
     let history = useHistory()
-
-    function goToOffice(){
+    function pickOfficeMode(){
         setChecked((prev) => !prev);
-        
     }
+
+    function goToOfficeAsLeader(){
+        history.push({
+            pathname: "/OfficeUi",
+            state: "Meeting Leader",
+          });
+        console.log("meetingLeader entered!")
+    }
+    function goToOfficeAsParticipant(){
+        history.push("/OfficeUi")
+    }
+
     function goToHome(){ 
         history.push("/HomeUi")
     }
     
-   
+
 
     const TypographyStyle = styled(Typography)`
         font-size: 24;
@@ -49,31 +80,39 @@ function HookTest() {
         color: black;
         font-family: "Arial Black", Arial Black;
     `
+    const TypographyStyleTwo = styled(Typography)`
+    font-size: 24;
+    font-weight: bold;
+    margin-top: -10%;
+    text-align: center;
+    color: black;
+    
+    font-family: "Arial Black", Arial Black;
+`
+    const IconStyle = styled(Icon)`
+        font-size: 40px;
+        
+        
 
+    `
     
 
     return (
         <div>
-        <Box sx={{width: "auto", height: "auto", display: 'flex', alignItems: "center", justifyContent: "center", flexWrap: 'wrap',
-            '& > :not(style)': {
-              m: 5,
-              width: 150,
-              height: 150,
-            }, }}>
+        <Box sx={{width: "auto", height: "auto", display: 'flex', alignItems: "center", justifyContent: "center", flexWrap: 'wrap', m: 10, gap: '8%' }}>
                 
                 <CenterRipple>
-                <PaperStyle elevation={5} onClick={goToOffice}><BusinessRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><TypographyStyle>Office</TypographyStyle></PaperStyle>
+                    <PaperStyle elevation={5} onClick={pickOfficeMode}><BusinessRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><TypographyStyle>Office</TypographyStyle></PaperStyle>
                 </CenterRipple>   
-                <Box sx={{ position: 'absolute', display: 'flex', alignItems: "bottom", paddingTop: '8%', paddingRight: '10%'}}>
-                    <Grow sx={{ width: 100, height: 100 }} in={checked}><AddCircleIcon/></Grow>
-                    <Grow sx={{ width: 100, height: 100 }} in={checked}><AddCircleIcon/></Grow>
+                <Box sx={{ position: 'absolute', display: 'flex', alignItems: "left", justifyContent: "left",  marginLeft: '-20%', marginTop: '20%'}}>
+                    <Grow sx={{ width: 240, height: 100 }} in={checked}><SmallPaper elevation={5} onClick={goToOfficeAsLeader} ><IconStyle icon="si-glyph:person-people" /><TypographyStyleTwo>Meeting Leader</TypographyStyleTwo></SmallPaper></Grow>
+                    <Grow sx={{ width: 240, height: 100 }} in={checked}><SmallPaper elevation={5} onClick={goToOfficeAsParticipant}><IconStyle icon="fluent:people-community-24-filled" /><TypographyStyleTwo>Participant</TypographyStyleTwo></SmallPaper></Grow>
 
                 </Box>
                 
                 <CenterRipple> 
                     <PaperStyle elevation={5} onClick={goToHome}><HomeWorkRoundedIcon sx={{ fontSize: 70, marginTop: '5%' }}/><TypographyStyle>Home</TypographyStyle></PaperStyle>
                 </CenterRipple>    
-            
         </Box>
         
         </div>
@@ -83,4 +122,9 @@ function HookTest() {
 
 }
 export default HookTest
-/*history.push("/OfficeUi")*/
+/*history.push("/OfficeUi")
+'& > :not(style)': {
+              m: 5,
+              width: 150,
+              height: 150,
+            },*/
